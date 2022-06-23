34fb6b59ad13d612b9e4500dfdb0ef1352d80835
Nakata Nobuyuki <nobu.nakata@gmail.com>
2022-06-23 14:16:00 +0900

    Add Test scripts

---
+++ Assets/Tests/TestTimeFormat.cs
@@ -0,0 +1,32 @@
+using NUnit.Framework;
+using System;
+using System.Globalization;
+using WeSyncSys.Extensions.TimeExt;
+
+public class TestTimeFormat {
+    [Test]
+    public void TestTimeFormatSimplePasses() {
+
+        var format = "yyyyMMddHHmmss";
+        var input = "20220622191433";
+
+        var nowUtc = DateTimeOffset.UtcNow;
+        var nowLocal = DateTimeOffset.Now;
+
+        Assert.True(input.TryParseAsDateTimeOffset(out var date, format));
+
+        Assert.AreEqual(2022, date.Year);
+        Assert.AreEqual(06, date.Month);
+        Assert.AreEqual(22, date.Day);
+        Assert.AreEqual(19, date.Hour);
+        Assert.AreEqual(14, date.Minute);
+        Assert.AreEqual(33, date.Second);
+
+        Assert.AreEqual(nowUtc.Offset, date.Offset);
+
+
+
+        Assert.True(input.TryParseAsDateTimeOffset(out date, format, true));
+        Assert.AreEqual(nowLocal.Offset, date.Offset);
+    }
+}
